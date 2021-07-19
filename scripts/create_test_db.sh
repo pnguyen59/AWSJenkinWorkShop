af3b0033661fc99e3f93a5413e833b66993c5f87
Nguyen <phucnct@a483e79591e1.ant.amazon.com>
2021-07-19 21:32:39 +0700

    fix

--- scripts/create_test_db.sh
+++ scripts/create_test_db.sh
@@ -1,6 +1,9 @@
 
 #!/bin/bash
-mysql -uroot <<CREATE_TEST_DB
-CREATE DATABASE IF NOT EXISTS test;
+mysql -u root <<CREATE_TEST_DB
+CREATE USER 'admin'@'localhost' IDENTIFIED BY '123456789';
+CREATE DATABASE `wordpress-db`;
+GRANT ALL PRIVILEGES ON `wordpress-db`.* TO "admin"@"localhost";
+FLUSH PRIVILEGES;
 CREATE_TEST_DB
 
